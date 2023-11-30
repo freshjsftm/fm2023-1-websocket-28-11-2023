a1028db168120947d894e3c6b2025b870e819674
Olena Zdanovska <freshjs@gmail.com>
2023-11-30 10:03:40 +0200

    Add condition render

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import ListMessages from '../components/ListMessages';
 import MessageForm from '../components/forms/MessageForm';
 import UserForm from '../components/forms/UserForm';
 
 const HomePage = () => {
+  const { user } = useSelector((store) => store.user);
   return (
     <div style={{ display: 'flex' }}>
       <div>
         <ListMessages />
-        <MessageForm />
+        {user && <MessageForm />}
       </div>
       <div>
-        <UserForm />
+        {!user && <UserForm />}
+        {user && <span>hi, {user.login}!</span>}
+        {user && <button>log out</button>}
       </div>
     </div>
   );
--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -3,9 +3,7 @@ const { User } = require('../models');
 module.exports.createOrFindUser = async (req, res, next) => {
   try {
     const { body } = req;
-    console.log(body.email);
     const [user] = await User.find({ email: body.email });
-    console.log(user);
     if (user) {
       return res.status(200).send({ data: user });
     }
